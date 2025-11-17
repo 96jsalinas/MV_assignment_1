4960bde676854bbd73ff142ca833fb237bfd91f6
Josu Salinas Colina <96jsalinas@gmail.com>
2025-11-17 15:16:16 +0100

    rename raw variable and fix bug

--- Preprocessing of Global Data.R
+++ Preprocessing of Global Data.R
@@ -1,47 +1,48 @@
 library(tidyverse)
 library(dplyr)
 
-raw <- read.csv("country_data.csv", header = TRUE, sep = ",")
+raw_data <- read.csv("country_data.csv", header = TRUE, sep = ",")
 
-raw <- raw %>%
+raw_data <- raw_data %>%
   select(name, region, population, everything())
 
-raw <- raw %>%
+raw_data <- raw_data %>%
   select(-surface_area, -imports, -employment_services, -employment_industry, -employment_agriculture,
          -forested_area, -exports, -threatened_species, -pop_growth, -pop_density,
          -refugees, -currency, -capital, -iso2)
 
-raw <- raw %>%
+raw_data <- raw_data %>%
   mutate(life_expectancy = rowMeans(across(c(life_expectancy_male, life_expectancy_female)), na.rm = FALSE))
 
-raw <- raw %>%
+raw_data <- raw_data %>%
   mutate(schooling = rowMeans(across(c(secondary_school_enrollment_female, secondary_school_enrollment_male,
                                        primary_school_enrollment_female, primary_school_enrollment_male,
                                        post_secondary_enrollment_female, post_secondary_enrollment_male)), na.rm = FALSE))
 
-raw <- raw %>%
+raw_data <- raw_data %>%
   select(-life_expectancy_male, -life_expectancy_female, -secondary_school_enrollment_female,
          -secondary_school_enrollment_male, -primary_school_enrollment_female, -primary_school_enrollment_male,
          -post_secondary_enrollment_female, -post_secondary_enrollment_male, -gdp, -urban_population_growth)
 
-raw <- raw %>%
-  select(name, region, population, na_count, everything())
-
-raw <- raw %>%
+raw_data <- raw_data %>%
   filter(population >= 100)
 
-raw <- raw %>%
+raw_data <- raw_data %>%
   mutate(
     na_count = rowSums(is.na(across(everything())))
   )
 
-hist(raw$schooling)
-hist(raw$homicide_rate,breaks = 50)
+raw_data <- raw_data %>%
+  select(name, region, population, na_count, everything())
 
-colSums(is.na(raw))
+hist(raw_data$schooling)
+hist(raw_data$homicide_rate,breaks = 50)
 
-raw <- raw %>%
+colSums(is.na(raw_data))
+
+raw_data <- raw_data %>%
   select(-co2_emissions)
+
 
 
 
