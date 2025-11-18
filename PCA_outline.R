02e834e85c1275fb8f3f44c5b9deef07be8bfc7f
Josu Salinas Colina <96jsalinas@gmail.com>
2025-11-18 12:28:09 +0100

    applied Air formatting to R files

--- PCA_outline.R
+++ PCA_outline.R
@@ -1,10 +1,15 @@
 source("preprocessing.R")
 
-#Select just quantitative variables 
+#Select just quantitative variables
 pca_data <- clean_data %>%
   select(
-    Life.expectancy, Adult.Mortality, thinness,
-    under.five.deaths, GDP, Population)
+    Life.expectancy,
+    Adult.Mortality,
+    thinness,
+    under.five.deaths,
+    GDP,
+    Population
+  )
 
 #Check correlation of data and covariance matrix
 cor(pca_data)
@@ -69,10 +74,10 @@ fviz_pca_ind(
 )
 
 #Loadings of features on each component
-pca_loadings <- pca$rotation    
+pca_loadings <- pca$rotation
 pca_loadings
 #Scores of each country for each component
-pca_scores   <- pca$x         
+pca_scores <- pca$x
 pca_scores
 
 #Selecting PC1 and PC2
@@ -85,21 +90,24 @@ fviz_contrib(pca, choice = "var", axes = 1)
 #Contribution of variables to PC2
 fviz_contrib(pca, choice = "var", axes = 2)
 #PCA weights
-loadings <- as.data.frame(pca$rotation[, 1:2])  # PC1 and PC2 only
+loadings <- as.data.frame(pca$rotation[, 1:2]) # PC1 and PC2 only
 loadings$Variable <- rownames(loadings)
 df_long <- loadings %>%
-  pivot_longer(cols = c(PC1, PC2),
-               names_to = "Component",
-               values_to = "Weight")
+  pivot_longer(cols = c(PC1, PC2), names_to = "Component", values_to = "Weight")
 ggplot(df_long, aes(x = Variable, y = Weight, fill = Component)) +
-  geom_bar(stat = "identity",
-           position = position_dodge(width = 0.6),
-           width = 0.4,                 # makes the bars skinny
-           colour = "black",            # black border
-           linewidth = 0.4) +           # border thickness
+  geom_bar(
+    stat = "identity",
+    position = position_dodge(width = 0.6),
+    width = 0.4, # makes the bars skinny
+    colour = "black", # black border
+    linewidth = 0.4
+  ) + # border thickness
   scale_fill_manual(values = c("PC1" = "blue", "PC2" = "red")) +
-  labs(title = "PCA Weights for PC1 and PC2",
-       x = "Variables", y = "Component Weight") +
+  labs(
+    title = "PCA Weights for PC1 and PC2",
+    x = "Variables",
+    y = "Component Weight"
+  ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 #Correlation of variables and PC1-3
@@ -107,17 +115,20 @@ cors <- cor(pca_data, pca$x[, 1:3])
 cors_df <- as.data.frame(cors)
 cors_df$Variable <- rownames(cors_df)
 cors_long <- cors_df %>%
-  pivot_longer(cols = starts_with("PC"),
-               names_to = "PC",
-               values_to = "Correlation")
+  pivot_longer(
+    cols = starts_with("PC"),
+    names_to = "PC",
+    values_to = "Correlation"
+  )
 ggplot(cors_long, aes(x = PC, y = Variable, fill = Correlation)) +
   geom_tile(color = "black") +
   scale_fill_gradient2(
-    low = "blue", mid = "white", high = "magenta",
+    low = "blue",
+    mid = "white",
+    high = "magenta",
     limits = c(-1, 1)
   ) +
-  labs(title = "Cross-correlations: variables vs PCs",
-       x = "", y = "") +
+  labs(title = "Cross-correlations: variables vs PCs", x = "", y = "") +
   theme_minimal() +
   theme(
     axis.text.x = element_text(angle = 0, hjust = 0.5),
--- preprocessing.R
+++ preprocessing.R
@@ -1,4 +1,4 @@
-rm(list=ls())
+rm(list = ls())
 library(tidyverse)
 library(dplyr)
 library("GGally")
@@ -9,29 +9,44 @@ library(tidyr)
 raw_data <- read.csv("Life Expectancy Data.csv", header = TRUE, sep = ",")
 
 #We will focus only on the data for the year 2015
-raw_data <- raw_data %>% filter(Year==2015) %>%
+raw_data <- raw_data %>%
+  filter(Year == 2015) %>%
   mutate(na_count = rowSums(is.na(.))) %>%
   arrange(na_count) %>%
   distinct(Country, .keep_all = TRUE)
 
 #Dropping columns that won't be used in analysis
-cols_to_drop <- c("Year", "Alcohol", "Hepatitis.B", "Measles", "BMI",
-                  "Total.expenditure", "Diphtheria", "HIV.AIDS",
-                  "na_count", "percentage.expenditure", "infant.deaths"
+cols_to_drop <- c(
+  "Year",
+  "Alcohol",
+  "Hepatitis.B",
+  "Measles",
+  "BMI",
+  "Total.expenditure",
+  "Diphtheria",
+  "HIV.AIDS",
+  "na_count",
+  "percentage.expenditure",
+  "infant.deaths"
 )
 clean_data <- raw_data %>%
   filter(Population >= 100000) %>% #Filtering out small countries
-  select(-all_of(cols_to_drop)) %>% mutate(na_count = rowSums(is.na(.))) #To check for columns with NA values
+  select(-all_of(cols_to_drop)) %>%
+  mutate(na_count = rowSums(is.na(.))) #To check for columns with NA values
 
 #Removing countries that don't have a GDP or Thinness data
-clean_data <- clean_data %>% filter(Country != "Papua New Guinea") %>% filter(Country != "Syrian Arab Republic")
-clean_data <- clean_data %>% filter(Country != "Sudan") %>% filter(Country != "South Sudan")
+clean_data <- clean_data %>%
+  filter(Country != "Papua New Guinea") %>%
+  filter(Country != "Syrian Arab Republic")
+clean_data <- clean_data %>%
+  filter(Country != "Sudan") %>%
+  filter(Country != "South Sudan")
 
 #Check for NAs
 colSums(is.na(clean_data))
 
 #We will combine Thinness data in a single variable by using a simple mean
-clean_data <- clean_data %>% 
+clean_data <- clean_data %>%
   mutate(
     thinness = (thinness..1.19.years + thinness.5.9.years) / 2
   )
@@ -45,7 +60,7 @@ hist(
 )
 
 #We will look at Polio to turn it into a binary
-#Polio := Polio (Pol3) immunization coverage among 1-year-olds (%) 
+#Polio := Polio (Pol3) immunization coverage among 1-year-olds (%)
 mean_val <- mean(clean_data$Polio, na.rm = TRUE)
 median_val <- median(clean_data$Polio, na.rm = TRUE)
 
@@ -58,10 +73,15 @@ hist(
   border = "white"
 )
 
-abline(v = mean_val, col = "red", lwd = 2)              #Mean in Red
-abline(v = median_val, col = "blue", lwd = 2, lty = 2)  #Median in Blue (dashed)
-legend("topright", legend = c("Mean", "Median"), 
-       col = c("red", "blue"), lwd = 2, lty = c(1, 2))
+abline(v = mean_val, col = "red", lwd = 2) #Mean in Red
+abline(v = median_val, col = "blue", lwd = 2, lty = 2) #Median in Blue (dashed)
+legend(
+  "topright",
+  legend = c("Mean", "Median"),
+  col = c("red", "blue"),
+  lwd = 2,
+  lty = c(1, 2)
+)
 
 #We can see that the mean is around 80%, which coincides with the 80% threshold that WHO sets for herd immunity
 #https://www.who.int/news-room/questions-and-answers/item/herd-immunity-lockdowns-and-covid-19/#:~:text=for%20Polio,%20the%20threshold%20is%20about%2080
@@ -71,7 +91,10 @@ clean_data$Polio_binary <- ifelse(clean_data$Polio > 80, 1, 0)
 ggplot(clean_data, aes(x = factor(Polio_binary), fill = factor(Polio_binary))) +
   geom_bar() +
   scale_x_discrete(labels = c("0" = "Below 80", "1" = "Above 80")) +
-  scale_fill_manual(values = c("0" = "salmon", "1" = "steelblue"), guide = "none") +
+  scale_fill_manual(
+    values = c("0" = "salmon", "1" = "steelblue"),
+    guide = "none"
+  ) +
   labs(
     title = "Count of Countries by Polio immunization category",
     subtitle = "Threshold: 80% Polio immunization among 1-year-olds",
@@ -82,10 +105,16 @@ ggplot(clean_data, aes(x = factor(Polio_binary), fill = factor(Polio_binary))) +
 
 #We will turn Developed into a 0/1 binary category
 clean_data$Status_binary <- ifelse(clean_data$Status == "Developed", 1, 0)
-ggplot(clean_data, aes(x = factor(Status_binary), fill = factor(Status_binary))) +
+ggplot(
+  clean_data,
+  aes(x = factor(Status_binary), fill = factor(Status_binary))
+) +
   geom_bar() +
   scale_x_discrete(labels = c("0" = "Developing", "1" = "Developed")) +
-  scale_fill_manual(values = c("0" = "salmon", "1" = "steelblue"), guide = "none") +
+  scale_fill_manual(
+    values = c("0" = "salmon", "1" = "steelblue"),
+    guide = "none"
+  ) +
   labs(
     title = "Count of Countries by Development",
     x = "Category",
@@ -94,7 +123,7 @@ ggplot(clean_data, aes(x = factor(Status_binary), fill = factor(Status_binary)))
   theme_minimal()
 
 
-#We will turn Schooling into a multicategory 
+#We will turn Schooling into a multicategory
 mean_val <- mean(clean_data$Schooling, na.rm = TRUE)
 median_val <- median(clean_data$Schooling, na.rm = TRUE)
 
@@ -107,14 +136,19 @@ hist(
   border = "white"
 )
 
-abline(v = mean_val, col = "red", lwd = 2)              #Mean in Red
-abline(v = median_val, col = "blue", lwd = 2, lty = 2)  #Median in Blue (dashed)
-legend("topright", legend = c("Mean", "Median"), 
-       col = c("red", "blue"), lwd = 2, lty = c(1, 2))
+abline(v = mean_val, col = "red", lwd = 2) #Mean in Red
+abline(v = median_val, col = "blue", lwd = 2, lty = 2) #Median in Blue (dashed)
+legend(
+  "topright",
+  legend = c("Mean", "Median"),
+  col = c("red", "blue"),
+  lwd = 2,
+  lty = c(1, 2)
+)
 max(clean_data$Schooling) #20.4
 min(clean_data$Schooling) #4.9
 #That gives us a range of 15.5, we will divide it into 5 categories
-clean_data <- clean_data %>% 
+clean_data <- clean_data %>%
   mutate(
     schooling_1to5 = cut(
       clean_data$Schooling,
@@ -147,8 +181,10 @@ clean_data <- clean_data %>%
   mutate(
     HDICat = case_when(
       Income.composition.of.resources < 0.55 ~ "low",
-      Income.composition.of.resources >= 0.55 & Income.composition.of.resources < 0.7 ~ "medium",
-      Income.composition.of.resources >= 0.7 & Income.composition.of.resources < 0.8 ~ "high",
+      Income.composition.of.resources >= 0.55 &
+        Income.composition.of.resources < 0.7 ~ "medium",
+      Income.composition.of.resources >= 0.7 &
+        Income.composition.of.resources < 0.8 ~ "high",
       Income.composition.of.resources > 0.8 ~ "very high"
     ),
     HDICat = factor(HDICat, levels = c("low", "medium", "high", "very high"))
@@ -163,14 +199,33 @@ ggplot(clean_data, aes(x = HDICat, fill = HDICat)) +
   theme_minimal()
 
 #We will remove columns that we transformed
-clean_data <- clean_data %>% select(-Polio, -Schooling, -Status, -Income.composition.of.resources, -na_count, -thinness..1.19.years, -thinness.5.9.years)
+clean_data <- clean_data %>%
+  select(
+    -Polio,
+    -Schooling,
+    -Status,
+    -Income.composition.of.resources,
+    -na_count,
+    -thinness..1.19.years,
+    -thinness.5.9.years
+  )
 
 #Summarize Quantitative columns (will be used in PCA)
-quant_columns <- c("Life.expectancy", "Adult.Mortality", "thinness", 
-                      "under.five.deaths", "GDP", "Population")
+quant_columns <- c(
+  "Life.expectancy",
+  "Adult.Mortality",
+  "thinness",
+  "under.five.deaths",
+  "GDP",
+  "Population"
+)
 summary_table <- clean_data %>%
   select(all_of(quant_columns)) %>%
-  pivot_longer(cols = everything(), names_to = "Variable", values_to = "Value") %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = "Variable",
+    values_to = "Value"
+  ) %>%
   group_by(Variable) %>%
   summarise(
     Min = min(Value, na.rm = TRUE),
